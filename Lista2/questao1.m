8560d395302a23b405a2edfa32a71c6927234dae
neto <euneto2501@gmail.com>
2022-08-28 23:24:14 -0300

    correcao na extracao da cor amarela da foto do gato

--- Lista2/questao1.m
+++ Lista2/questao1.m
@@ -1,5 +1,7 @@
 gato = imread('C:\Users\eucli\Documents\aulas\PDI\PDI\Lista2\gato.jpg');
 
+gatoAmarelo = uint8(zeros(size(gato,1), size(gato,2), size(gato,3)));
+
 for(i=1:1:size(gato,1)) 
   for(j=1:1:size(gato,2))
     if(gato(i,j,1)> 126 && gato(i,j,2) > 126 && gato(i,j,3) < 126)
